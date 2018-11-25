82c0e9d03d8cdcbc6d0de27a4da9643d3a968518
Timothy Tan <timtan93@gmail.com>
2018-11-25 00:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeli, name) {
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
 }
 
-function nowServing(katzDeliLine){
+function nowServing(katzDeli){
   if (katzDeli.length > 0) {
     return `Currently serving ${katzDeli.shift()}.`;
   } else if (katzDeli.length === 0) {
