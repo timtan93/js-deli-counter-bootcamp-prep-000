0f74555478643938aa9dd7da197862d912e42e4a
Timothy Tan <timtan93@gmail.com>
2018-11-25 00:23:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function nowServing(katzDeliLine){
     return 'There is nobody waiting to be served!';
   }
 }
+function currentLine(katzDeli) {
+  if (katzDeli.length > 0) {
+    var string = `The line is currently: 1. ${katzDeli[0]}`;
+    for (let i = 1; i < katzDeli.length; i++) {
+      string += `, ${i + 1}. ${katzDeli[i]}`;
+    }
+    return string;
+  } else if (katzDeli.length === 0) {
+    return 'The line is currently empty.';
+  }
+}
